887c3005fe2e513f484c90c930b767964c1a0919
flykatana001 <159651060+flykatana001@users.noreply.github.com>
2026-04-03 14:03:43 +0200

    Refactor SeplosBmsBle class for improved readability

--- components/seplos_bms_ble/seplos_bms_ble.cpp
+++ components/seplos_bms_ble/seplos_bms_ble.cpp
@@ -1,6 +1,5 @@
 #include "seplos_bms_ble.h"
 #include "esphome/core/log.h"
-#include "esphome/core/component.h"
 
 namespace esphome {
 namespace seplos_bms_ble {
@@ -12,6 +11,9 @@ void SeplosBmsBle::dump_config() {
 }
 
 void SeplosBmsBle::loop() {
+  if (this->node_state != ble_client::ClientState::ESTABLISHED)
+    return;
+    
   const uint32_t now = millis();
   if (now - last_poll_ > 3000) {
     poll_status();
@@ -19,9 +21,24 @@ void SeplosBmsBle::loop() {
   }
 }
 
+void SeplosBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) {
+  switch (event) {
+    case ESP_GATTC_NOTIFY_EVT: {
+      if (param->notify.conn_id != this->conn_id_)
+        break;
+      if (param->notify.handle != this->char_handle_)
+        break;
+      this->on_ble_data(std::vector<uint8_t>(param->notify.value, param->notify.value + param->notify.value_len));
+      break;
+    }
+    default:
+      break;
+  }
+}
+
 void SeplosBmsBle::poll_status() {
   uint8_t frame[] = {0xDD, 0xA5, 0x03, 0x00, 0xFF, 0xFD, 0x77};
-  this->parent()->write_value(frame, sizeof(frame));
+  this->write(frame, sizeof(frame));
 }
 
 void SeplosBmsBle::on_ble_data(const std::vector<uint8_t> &data) {
@@ -36,19 +53,19 @@ void SeplosBmsBle::on_ble_data(const std::vector<uint8_t> &data) {
   int cycles = (p[8] << 8) | p[9];
   int cell_count = p[19];
 
-  if (voltage_sensor) voltage_sensor->publish_state(voltage);
-  if (current_sensor) current_sensor->publish_state(current);
-  if (soc_sensor) soc_sensor->publish_state(soc);
-  if (cycle_sensor) cycle_sensor->publish_state(cycles);
+  if (voltage_sensor_) voltage_sensor_->publish_state(voltage);
+  if (current_sensor_) current_sensor_->publish_state(current);
+  if (soc_sensor_) soc_sensor_->publish_state(soc);
+  if (cycle_sensor_) cycle_sensor_->publish_state(cycles);
 
   int cell_start = 21;
-  for (int i = 0; i < cell_count && i < cell_sensors.size(); i++) {
+  for (int i = 0; i < cell_count && i < cell_sensors_.size(); i++) {
     float cv = ((p[cell_start + i*2] << 8) | p[cell_start + i*2 + 1]) / 1000.0f;
-    cell_sensors[i]->publish_state(cv);
+    if (i < cell_sensors_.size())
+      cell_sensors_[i]->publish_state(cv);
   }
 }
 
 }  // namespace seplos_bms_ble
 }  // namespace esphome
 
-REGISTER_COMPONENT(esphome::seplos_bms_ble::SeplosBmsBle, "seplos_bms_ble");
