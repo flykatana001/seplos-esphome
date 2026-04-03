025088410f34f8e68d832bad5ee1a4deae611fdc
flykatana001 <159651060+flykatana001@users.noreply.github.com>
2026-04-03 14:17:03 +0200

    Update seplos_bms_ble.h

--- components/seplos_bms_ble/seplos_bms_ble.h
+++ components/seplos_bms_ble/seplos_bms_ble.h
@@ -24,6 +24,7 @@ class SeplosBmsBle : public ble_client::BLEClientNode, public Component {
   void poll_status();
   uint32_t last_poll_{0};
   bool connected_{false};
+  uint16_t write_handle_{0};
   
   sensor::Sensor *voltage_sensor_{nullptr};
   sensor::Sensor *current_sensor_{nullptr};
