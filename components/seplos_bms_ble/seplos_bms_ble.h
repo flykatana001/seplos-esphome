22e0efaae00650077e0f1d67fc44623ca6a18713
flykatana001 <159651060+flykatana001@users.noreply.github.com>
2026-04-03 14:10:02 +0200

    Update seplos_bms_ble.h

--- components/seplos_bms_ble/seplos_bms_ble.h
+++ components/seplos_bms_ble/seplos_bms_ble.h
@@ -11,19 +11,19 @@ class SeplosBmsBle : public ble_client::BLEClientNode, public Component {
  public:
   void dump_config() override;
   void loop() override;
-  
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp_ble_gattc_cb_param_t *param) override;
 
-  void set_voltage_sensor(sensor::Sensor *voltage_sensor) { voltage_sensor_ = voltage_sensor; }
-  void set_current_sensor(sensor::Sensor *current_sensor) { current_sensor_ = current_sensor; }
-  void set_soc_sensor(sensor::Sensor *soc_sensor) { soc_sensor_ = soc_sensor; }
-  void set_cycle_sensor(sensor::Sensor *cycle_sensor) { cycle_sensor_ = cycle_sensor; }
-  void add_cell_sensor(sensor::Sensor *cell_sensor) { cell_sensors_.push_back(cell_sensor); }
+  void set_voltage_sensor(sensor::Sensor *sensor) { voltage_sensor_ = sensor; }
+  void set_current_sensor(sensor::Sensor *sensor) { current_sensor_ = sensor; }
+  void set_soc_sensor(sensor::Sensor *sensor) { soc_sensor_ = sensor; }
+  void set_cycle_sensor(sensor::Sensor *sensor) { cycle_sensor_ = sensor; }
+  void add_cell_sensor(sensor::Sensor *sensor) { cell_sensors_.push_back(sensor); }
 
  protected:
   void on_ble_data(const std::vector<uint8_t> &data);
   void poll_status();
   uint32_t last_poll_{0};
+  bool connected_{false};
   
   sensor::Sensor *voltage_sensor_{nullptr};
   sensor::Sensor *current_sensor_{nullptr};
